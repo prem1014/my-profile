980e9110f814ee11254d590815412bae898d6310
Prem Prakash <prem1014prakash@gmail.com>
2018-01-23 23:21:00 +0530

    committed ang-basics

--- src/app/dashboard/angular-tutorial/ang-basics/ang-basics.ts
+++ src/app/dashboard/angular-tutorial/ang-basics/ang-basics.ts
@@ -2,7 +2,8 @@ import { Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-ang-basics',
-    templateUrl: './ang-basics.html'
+    templateUrl: './ang-basics.html',
+    styleUrls: ['./ang-basics.scss']
 })
 
 export class AngBasicsComponent implements OnInit {

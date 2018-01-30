e3c9189c2703083a7d1eefd11d53071d1439271d
Prem Prakash <prem1014prakash@gmail.com>
2018-01-30 23:04:45 +0530

    added tutorials

--- src/app/dashboard/angular-tutorial/ang-basics/ang-basics.ts
+++ src/app/dashboard/angular-tutorial/ang-basics/ang-basics.ts
@@ -11,10 +11,17 @@ export class AngBasicsComponent implements OnInit {
     constructor(){
         this.buildingsBlocks = [
             {
-                name: 'Modules'
+                name: 'Modules',
+                intro: 'Angular apps are modular and it has at least one module that acts as '+ 
+                'root module. Angular has its own modularity system called NgModules. One '+ 
+                'can divide an app into several feature modules and all modules will be part of '+ 
+                'main root module.An NgModule, whether a root or feature, is a class with an @NgModule decorator.'
             },
             {
-                name: 'Components'
+                name: 'Components',
+                intro: 'A component controls the view part of an application. '+
+                'You can define the business logic inside the component class to control the view. '+
+                'Angular creates, updates, and destroys components as the user moves through the application.'
             },
             {
                 name: 'Templates'
